9da82d23d23ad8cf7366f3ed486415b00db8b086
colin gaming <n.wells3@icloud.com>
2018-10-04 15:32:49 -0400

    ok

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 if(process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
-}
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
